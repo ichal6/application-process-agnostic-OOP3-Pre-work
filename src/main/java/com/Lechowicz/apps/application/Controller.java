29395d1b6e543d4f56b757f52dcc5b092be0d3d8
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-03-26 16:41:37 +0100

    Write application package

--- src/main/java/com/Lechowicz/apps/application/Controller.java
+++ src/main/java/com/Lechowicz/apps/application/Controller.java
@@ -1,5 +1,13 @@
 package com.Lechowicz.apps.application;
 
-public class Controller {
+import com.Lechowicz.apps.interactions.*;
 
+public class Controller {
+    View view = new TerminalView();
+    InputManager input = new TerminalInput();
+
+    private void answerOne(){
+        view.print("Write a query that returns the 2 name columns of the mentors table\n");
+
+    }
 }
--- src/main/java/com/Lechowicz/apps/application/Main.java
+++ src/main/java/com/Lechowicz/apps/application/Main.java
@@ -2,6 +2,6 @@ package com.Lechowicz.apps.application;
 
 public class Main {
     public static void main(String[] args){
-
+        new Controller();
     }
 }
