23f903be9b6e348199401973c7901eb56d57807e
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-03-27 19:01:58 +0100

    Write Model class

--- src/main/java/com/Lechowicz/apps/logic/Model.java
+++ src/main/java/com/Lechowicz/apps/logic/Model.java
@@ -1,4 +1,11 @@
 package com.Lechowicz.apps.logic;
 
+import com.Lechowicz.apps.persons.Person;
+
+import java.util.List;
+
 public class Model {
+    public List<Person> getFullNameMentors() {
+        return null;
+    }
 }
