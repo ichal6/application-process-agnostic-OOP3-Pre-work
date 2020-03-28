77ae1893b7ddf3b30d04d9f66beaab77e3e975e8
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-03-28 15:35:26 +0100

    Implement body of Candidate

--- src/main/java/com/Lechowicz/apps/persons/Candidate.java
+++ src/main/java/com/Lechowicz/apps/persons/Candidate.java
@@ -1,4 +1,15 @@
 package com.Lechowicz.apps.persons;
 
 public class Candidate extends Person {
+    protected Integer applicationCode;
+
+    public Candidate(String[] infoAboutPerson) {
+        super(infoAboutPerson);
+
+        this.applicationCode = Integer.parseInt(infoAboutPerson[6]);
+    }
+
+    public Integer getApplicationCode() {
+        return applicationCode;
+    }
 }
