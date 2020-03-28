5d71e74da4b5fcbe4c85440c33f9dc02ecbe2878
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-03-28 15:07:20 +0100

    Implement body of Person

--- src/main/java/com/Lechowicz/apps/persons/Person.java
+++ src/main/java/com/Lechowicz/apps/persons/Person.java
@@ -1,4 +1,41 @@
 package com.Lechowicz.apps.persons;
 
 public abstract class Person {
+    protected Integer id;
+    protected String firstName;
+    protected String lastName;
+    protected String phoneNumber;
+    protected String email;
+
+    public String getFullName(){
+        return firstName + " " + lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
