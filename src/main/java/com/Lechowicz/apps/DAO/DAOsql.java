72e11c433fcc454cdff22a0686e0ec118173f208
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-03-28 18:19:30 +0100

    Implement load from sql

--- src/main/java/com/Lechowicz/apps/DAO/DAOsql.java
+++ src/main/java/com/Lechowicz/apps/DAO/DAOsql.java
@@ -1,16 +1,87 @@
 package com.Lechowicz.apps.DAO;
 
+import com.Lechowicz.apps.persons.Candidate;
 import com.Lechowicz.apps.persons.Mentor;
 import com.Lechowicz.apps.persons.Person;
+import org.apache.ibatis.jdbc.ScriptRunner;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DAOsql implements InterfaceDAO {
     private List<Person> mentors;
     private List<Person> candidates;
 
-    public DAOsql(){
+    private String url;
+    private String user;
+    private String password;
 
+    public DAOsql() throws IOException{
+        Properties prop = readProperties("src/main/resources/database.properties");
+        url = prop.getProperty("db.url");
+        user = prop.getProperty("db.user");
+        password = prop.getProperty("db.passwd");
+
+        mentors = new ArrayList<>();
+        candidates = new ArrayList<>();
+        openDB();
+    }
+
+    public static Properties readProperties(String path) throws IOException {
+
+        Properties props = new Properties();
+        Path myPath = Paths.get(path);
+
+        BufferedReader bf = Files.newBufferedReader(myPath, StandardCharsets.UTF_8);
+        props.load(bf);
+
+        return props;
+    }
+
+    private void openDB() {
+        String[] params = new String[8];
+        int lengthOfCandidate = 6;
+        int lengthOfMentors = 8;
+        try {
+
+            Connection con = DriverManager.getConnection(url, user, password);
+            PreparedStatement pst = con.prepareStatement("SELECT * FROM applicants");
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+                for(int index = 0; index < lengthOfCandidate;index++){
+                    params[index] = rs.getString(index + 1);
+                }
+                candidates.add(new Candidate(params));
+            }
+
+            pst = con.prepareStatement("SELECT * FROM mentors");
+            rs = pst.executeQuery();
+
+            while (rs.next()) {
+                for(int index = 0; index < lengthOfMentors;){
+                    params[index] = rs.getString(index + 1);
+                    index++;
+                }
+                mentors.add(new Mentor(params));
+            }
+
+        } catch (SQLException ex) {
+
+            System.out.println("Error in Database" + ex);
+        }
     }
 
     @Override
@@ -50,8 +121,4 @@ public class DAOsql implements InterfaceDAO {
 
     }
 
-
-    private void readFromDB(){
-
-    }
 }
