42fa6ffd8a398fc6e21b2219140ce0e59fdbf442
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-03-28 16:09:34 +0100

    Write sheleton of DAOsql

--- src/main/java/com/Lechowicz/apps/DAO/DAOsql.java
+++ src/main/java/com/Lechowicz/apps/DAO/DAOsql.java
@@ -1,4 +1,57 @@
 package com.Lechowicz.apps.DAO;
 
+import com.Lechowicz.apps.persons.Mentor;
+import com.Lechowicz.apps.persons.Person;
+
+import java.util.List;
+
 public class DAOsql implements InterfaceDAO {
+    private List<Person> mentors;
+    private List<Person> candidates;
+
+    DAOsql(){
+
+    }
+
+    @Override
+    public List<Person> getMentors() {
+        return mentors;
+    }
+
+    @Override
+    public List<Person> getCandidates() {
+        return candidates;
+    }
+
+    @Override
+    public void updatePerson(String fullName, Person person) {
+        List<Person> searchList;
+        if(person instanceof Mentor){
+          searchList = mentors;
+        }
+        else{
+            searchList = candidates;
+        }
+        for(Person singlePerson: searchList){
+            if(fullName.equals(singlePerson.getFullName())){
+                searchList.remove(singlePerson);
+                searchList.add(person);
+            }
+        }
+    }
+
+    @Override
+    public void deletePerson(String fullName) {
+
+    }
+
+    @Override
+    public void addPerson(String fullName) {
+
+    }
+
+
+    private void readFromDB(){
+
+    }
 }
