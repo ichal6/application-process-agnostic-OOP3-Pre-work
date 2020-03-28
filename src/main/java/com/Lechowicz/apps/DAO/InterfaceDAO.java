984ea97fbea1e297bfbde1a6832d5a6348f0a60c
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-03-28 16:09:15 +0100

    Write interface DAO

--- src/main/java/com/Lechowicz/apps/DAO/InterfaceDAO.java
+++ src/main/java/com/Lechowicz/apps/DAO/InterfaceDAO.java
@@ -1,4 +1,18 @@
 package com.Lechowicz.apps.DAO;
 
+import com.Lechowicz.apps.persons.Person;
+
+import java.util.List;
+
 public interface InterfaceDAO {
+
+    public List<Person> getMentors();
+
+    public List<Person> getCandidates();
+
+    public void updatePerson(String fullName, Person person);
+
+    public void deletePerson(String fullName);
+
+    public void addPerson(String fullName);
 }
