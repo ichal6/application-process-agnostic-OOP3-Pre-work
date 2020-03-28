39910386084d66740ba2a6469cdfb60797b3c00e
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-03-28 15:31:36 +0100

    Implement body of Mentor

--- src/main/java/com/Lechowicz/apps/persons/Mentor.java
+++ src/main/java/com/Lechowicz/apps/persons/Mentor.java
@@ -1,4 +1,22 @@
 package com.Lechowicz.apps.persons;
 
 public class Mentor extends Person {
+    private String city;
+    private Integer favouriteNumber;
+
+    public Mentor(String[] param) {
+        super(param);
+        city = param[5];
+        favouriteNumber = Integer.parseInt(param[6]);
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Integer getFavouriteNumber() {
+        return favouriteNumber;
+    }
+
+
 }
